7dd45080425ea189e2f599b920505cb73ca3a3dc
sisrfeng <liuweifengemail@163.com>
2021-09-27 11:49:57 +0800

    wf

--- sources_china_ubuntu20.py
+++ sources_china_ubuntu20.py
@@ -1,24 +1,14 @@
-# ubuntu 18
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
-deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-proposed main restricted universe multiverse
+# ubuntu 20
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse
+deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse
 
-
-#############ubuntu           16  ============================
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial main restricted
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates main restricted
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial universe
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates universe
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial multiverse
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-updates multiverse
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-backports main restricted universe multiverse
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security main restricted
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security universe
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ xenial-security multiverse
+# 预发布软件源，不建议启用
+# deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
+# deb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-proposed main restricted universe multiverse
