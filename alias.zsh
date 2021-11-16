37d40246d47f98c28dc834835fa2928e9f372e6f
sisrfeng <liuweifengemail@163.com>
2021-11-16 11:39:29 +0800

    新git顺序_by_leo

--- alias.zsh
+++ alias.zsh
@@ -528,7 +528,9 @@ alias snp='~/dot_file/wf_snippet.py'
 # alias gip='git add . ; git commit -m "wf" ; git push ; cd - ; zsh'
 sy(){
     cd ~/dot_file 
-    git pull 
+    git stash
+    git pull  # Update the branch to the latest code .   = fetch + merge? 还是只fetch?  
+    git stash apply  # Merge your local changes into the latest code:
     git add --verbose .
     git commit -m "$1_by_leo" 
     git push 
