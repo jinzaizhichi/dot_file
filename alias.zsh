e1f567d19ddc4a1e59ccac1d7de6895afeecff47
sisrfeng <liuweifengemail@163.com>
2021-12-26 12:28:12 +0800

    commit名：12月26日12:28

--- alias.zsh
+++ alias.zsh
@@ -790,10 +790,10 @@ y(){
     git stash
     echo "\n-----------------2. pull, 拉远程的新代码-----------------"
     git pull  # Update the branch to the latest code   = fetch + merge? 还是只fetch?
-    echo "\n--此时，本地的修改还是被藏着，一般打开文件是找不到修改的"
+    echo "\n此时，本地的修改还被藏着，一般打开文件是找不到修改的"
     echo "\n---------------------3. stashed的东西并到 本地的当前代码 ---------------------"
     git stash apply  # Merge your local changes into the latest code:
-    echo " 检查一下有没有冲突 "
+    echo "\n 亲，检查一下有没有冲突 "
 }
 
 yy(){
